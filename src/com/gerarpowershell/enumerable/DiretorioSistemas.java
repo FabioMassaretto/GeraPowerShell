71eb5f16a0f6f567cb48d4d60523de203183107e
FABIO MASSARETTO <fmassaretto@gmail.com>
2018-07-03 00:45:00 -0300

    Adding one more parameter to enum

--- src/com/gerarpowershell/enumerable/DiretorioSistemas.java
+++ src/com/gerarpowershell/enumerable/DiretorioSistemas.java
@@ -1,17 +1,18 @@
 package com.gerarpowershell.enumerable;
 
 public enum DiretorioSistemas {
-	MCS("MCS", "D:\\inetpub\\MCS\\MapfreConnectSite"),
-	NCC("NCC", "D:\\inetpub\\MCS\\MapfreConnectSite"),
-	OUTRO("OUTRO", "OUTRO");
-	;
+	MCS("MCS", "D:\\inetpub\\MCS\\MapfreConnectSite", "D:\\temp"),
+	NCC("NCC", "C:\\inetpub\\NCC\\Site", "C:\\temp"),
+	OUTRO("OUTRO", "OUTRO", "");
 	
 	private String diretorio;
 	private String sistema;
+	private String dirPacote;
 
-	DiretorioSistemas(String sistema, String diretorio) {
+	DiretorioSistemas(String sistema, String dirSistema, String dirPacote) {
 		this.sistema = sistema;
-		this.diretorio = diretorio;
+		this.diretorio = dirSistema;
+		this.dirPacote = dirPacote;
 	}
 	
 	public String getDiretorio() {
@@ -20,6 +21,10 @@ public enum DiretorioSistemas {
 
 	public String getSistema() {
 		return sistema;
+	}
+
+	public String getDirPacote() {
+		return dirPacote;
 	}
 
 }
